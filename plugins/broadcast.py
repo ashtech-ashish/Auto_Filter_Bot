42d78e80c0571bb56dc912844ddfce7bfe7c9e32
MandaliWala <141039684+MandaliWala@users.noreply.github.com>
2025-09-03 22:04:18 +0530

    forgot the main thing

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -197,7 +197,7 @@ async def clear_junk(user_id, message):
 
 async def broadcast_messages(user_id, message):
     try:
-        await message.copy(chat_id=user_id)
+        await message.forward(chat_id=user_id)
         return True, "Success"
     except FloodWait as e:
         await asyncio.sleep(e.value)
