fc135178bd4ba746f5ab71100e3d04acf76fdc9f
ashtech-ashish <ash.kum.2407@gmail.com>
2025-09-17 20:01:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/cyf.jpg'))
 FSUB_IMG = (environ.get('FSUB_IMG', 'https://envs.sh/cyO.jpg')).split() 
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5457101197 5713620365 6580023819').split()]  #Admin Id
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002151484893 -1002595119582 -1002075429339 -1002888854366').split()] #Movie Database Channel Id
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002151484893 -1002595119582 -1002075429339').split()] #Movie Database Channel Id
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002679083488'))  #Log Channel Id
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002627598818'))  #Streming Log Channel Id
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002552452659'))  #Movie Update Channel Id
